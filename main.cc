85f66ecd64de3a1f6d881e7c9454e54ead9d7462
ParticleG <particle_g@outlook.com>
2024-08-04 10:59:04 +0800

    - Fix compile error

--- main.cc
+++ main.cc
@@ -1,7 +1,11 @@
+#include <format>
+
 #include <drogon/drogon.h>
 
+using namespace std;
+
 int main() {
-    LOG_INFO << std::format("{} v{} starting...", CMAKE_PROJECT_NAME, VERSION_STRING);
+    LOG_INFO << format("{} v{} starting...", CMAKE_PROJECT_NAME, VERSION_STRING);
     drogon::app().loadConfigFile("config.json");
     drogon::app().run();
     return 0;
