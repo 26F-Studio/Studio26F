818532e6601b2a3c77025544bf64b52a69899baf
ParticleG <particle_g@outlook.com>
2024-08-04 09:10:40 +0800

    - Update version - Update vcpkg - Add version string to CMakeLists.txt - Init workflow

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <drogon/drogon.h>
 
 int main() {
+    LOG_INFO << std::format("{} v{} starting...", CMAKE_PROJECT_NAME, VERSION_STRING);
     drogon::app().loadConfigFile("config.json");
     drogon::app().run();
     return 0;
